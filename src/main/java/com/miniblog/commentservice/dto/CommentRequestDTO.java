f86dbcc3642c6290ae5e7f9198b7ef3458d12d95
DiilaNa <liyanaarachchidilan@gmail.com>
2026-08-30 12:54:24 +0530

    fix: update services

--- src/main/java/com/miniblog/commentservice/dto/CommentRequestDTO.java
+++ src/main/java/com/miniblog/commentservice/dto/CommentRequestDTO.java
@@ -18,7 +18,7 @@ public class CommentRequestDTO {
 
     // Getters and Setters
     public Long getPostId() { return postId; }
-    public void setPostId(Long postId) { this.postId = postId; }
+    public void setPostId(Long postId) { this.postId = postId;   }
 
     public Long getUserId() { return userId; }
     public void setUserId(Long userId) { this.userId = userId; }
